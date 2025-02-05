73a6b49d1eb6ef8bf68c43498c491ee76edfdfda
P0Ci <febriandry58@gmail.com>
2025-02-05 14:13:43 +0700

    Make DB Postgres

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
-import {
-	_ "database/sql"
-
-	_ "github.com/lib_pq"
-}
+import (
+    "database/sql"
+    "log"
+    "github.com/gin-gonic/gin"
+    _ "github.com/lib/pq"
+)
 
 type newStudent struct {
-	Student_id				uint64 `json:"student_id" binding:"required"`
-	Student_name			uint64 `json:"student_id" binding:"required"`
-	Student_age				uint64 `json:"student_id" binding:"required"`
-	Student_address			uint64 `json:"student_id" binding:"required"`
-	Student_phone_no		uint64 `json:"student_id" binding:"required"`
+    Student_id      uint64 `json:"student_id" binding:"required"`
+    Student_name    string `json:"student_name" binding:"required"`
+    Student_age     uint64 `json:"student_age" binding:"required"`
+    Student_address string `json:"student_address" binding:"required"`
+    Student_phone_no string `json:"student_phone_no" binding:"required"`
+}
+
+func setRouter() *gin.Engine {
+    conn := "postgresql://postgres:postgre@127.0.0.1/postgres?sslmode=disable"
+    
+    db, err := sql.Open("postgres", conn)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+    
+    r := gin.Default()
+    return r
+}
+
+func main() {
+    r := setRouter()
+    r.Run(":5020")
 }
 
